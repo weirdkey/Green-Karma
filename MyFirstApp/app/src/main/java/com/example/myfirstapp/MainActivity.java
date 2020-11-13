5c4c6803341b6ef2717bb986924e91012f962358
ge49kan@mytum.de <ge49kan@mytum.de>
2020-11-14 00:32:13 +0100

    added button functionality

--- MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                final EditText tv1 = findViewById(R.id.editTextText1);
+                final EditText tv2 = findViewById(R.id.editTextText2);
+                CharSequence username = tv1.getText();
+                CharSequence password = tv2.getText();
+                if (Verificator.verificate(username,password)){
+                    setContentView(R.layout.activity_secondary);
+                }
+            }
+        });
     }
 }
