6dac83477f59140a8ca71a24d8347eea4661da81
ge49kan@mytum.de <ge49kan@mytum.de>
2020-11-14 18:51:21 +0100

    prepare your merge conflicts

--- MyFirstApp/app/src/main/java/com/example/myfirstapp/UserProfile.java
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/UserProfile.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.view.View;
 
 public class UserProfile  extends Activity implements View.OnClickListener{
+
+
+
     @Override
     public void onClick(View v) {
 
