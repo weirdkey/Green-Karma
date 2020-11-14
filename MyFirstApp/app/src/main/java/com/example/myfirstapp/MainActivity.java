d1c9de1f1c9b3a0eb5fabd8b98cbf89395a5a5d1
ge49kan@mytum.de <ge49kan@mytum.de>
2020-11-14 09:51:09 +0100

    merge conflict rado

--- MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
+    private Button login_button;
+    private Button login_button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,19 +19,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        final Button button = findViewById(R.id.login_button);
+        this.login_button = findViewById(R.id.login_button);
 
-        button.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                final EditText tv1 = findViewById(R.id.login_email);
-                final EditText tv2 = findViewById(R.id.login_password);
-                CharSequence username = tv1.getText();
-                CharSequence password = tv2.getText();
-                if (Verificator.verificate(username,password)){
-                    setContentView(R.layout.activity_secondary);
-                }
+        login_button.setOnClickListener(v -> {
+            final EditText tv1 = findViewById(R.id.login_email);
+            final EditText tv2 = findViewById(R.id.login_password);
+            CharSequence username = tv1.getText();
+            CharSequence password = tv2.getText();
+            if (Verificator.verificate(username, password)) {
+                setContentView(R.layout.activity_secondary);
             }
         });
+
+        this.login_button2 = findViewById(R.id.login_button2);
+        login_button2.setOnClickListener(this);
+
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.login_button:
+                Intent switchLayout = new Intent(this, RegisterActivity.class);
+                startActivity(switchLayout);
+        }
+    }
+
 }
