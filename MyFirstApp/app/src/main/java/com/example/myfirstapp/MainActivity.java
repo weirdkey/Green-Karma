6f550a67c0870379db5cf42e8ca8ebbf55d84223
ge36qij <ge36qij@mytum.de>
2020-11-14 09:19:02 +0200

    merge conflict1234

--- MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,13 +15,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        final Button button = findViewById(R.id.button);
+        final Button button = findViewById(R.id.login_button);
 
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                final EditText tv1 = findViewById(R.id.editTextText1);
-                final EditText tv2 = findViewById(R.id.editTextText2);
+                final EditText tv1 = findViewById(R.id.login_email);
+                final EditText tv2 = findViewById(R.id.login_password);
                 CharSequence username = tv1.getText();
                 CharSequence password = tv2.getText();
                 if (Verificator.verificate(username,password)){
