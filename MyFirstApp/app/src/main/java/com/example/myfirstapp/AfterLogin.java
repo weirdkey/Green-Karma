c42dc67bfa3dd62aec8b9ad3c185b08418414603
ge36qij <ge36qij@mytum.de>
2020-11-14 17:32:43 +0200

    merge conflict1234567891

--- MyFirstApp/app/src/main/java/com/example/myfirstapp/AfterLogin.java
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/AfterLogin.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.graphics.ColorFilter;
 import android.graphics.LightingColorFilter;
+import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.DrawableContainer;
 import android.graphics.drawable.GradientDrawable;
@@ -13,6 +14,7 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 import com.google.android.material.bottomnavigation.BottomNavigationMenu;
